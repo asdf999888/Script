12ee8e1537a30586fcfe517b5c1437bd66a16fc0
asdf999888 <74891670+asdf999888@users.noreply.github.com>
2020-12-15 11:27:48 +0800

    Update meiriwaikan.js

--- meiriwaikan.js
+++ meiriwaikan.js
@@ -2,7 +2,7 @@
 每日外壳内解锁内容
 [rewrite_local]
 # Xunjie Unlock annual subscriptions （by LTribe）
-^https?:\/\/.dict\.eudic\.bet\/jingting\/v\d\/* url script-response-body xunjie.js
+^https?:\/\/.dict\.eudic\.bet\/jingting\/v\d\/* url script-response-body https://cdn.jsdelivr.net/gh/asdf999888/Script@patch-1/meiriwaikan.js
 [mitm]
 hostname = dict.eudic.net,
 */
