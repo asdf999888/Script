4b679ed0fa89b2de0ff9dd50b5f46fecc05ed594
asdf999888 <74891670+asdf999888@users.noreply.github.com>
2020-12-15 11:34:40 +0800

    Update meiriwaikan.js

--- meiriwaikan.js
+++ meiriwaikan.js
@@ -1,5 +1,5 @@
 /*Quantumult X 脚本:
-每日外壳内解锁内容
+每日外刊内解锁内容
 [rewrite_local]
 # Xunjie Unlock annual subscriptions （by LTribe）
 ^https?:\/\/.dict\.eudic\.bet\/jingting\/v\d\/* url script-response-body https://cdn.jsdelivr.net/gh/asdf999888/Script@patch-1/meiriwaikan.js
